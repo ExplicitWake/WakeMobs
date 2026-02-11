1beea825ac516a7e61cbe6c66bf1b1eeffa11473
ExplicitWake <pixelgunmyworldxd@gmail.com>
2026-02-11 13:26:15 +0300

    changed the logic of the command

--- src/main/java/ru/awake/wakemobs/commands/ReloadCommand.java
+++ src/main/java/ru/awake/wakemobs/commands/ReloadCommand.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.Nullable;
 import ru.awake.wakemobs.WakeMobs;
 import ru.awake.wakemobs.config.Config;
 
+import java.io.File;
 import java.util.List;
 
 public class ReloadCommand implements TabExecutor {
@@ -26,21 +27,20 @@ public class ReloadCommand implements TabExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
         if (commandSender.isOp()) {
-            if (strings[0].equals("reload")) {
+            if (strings.length == 1 && strings[0].equals("reload")) {
                 wakeMobs.getServer().getScheduler().runTaskAsynchronously(wakeMobs, () -> {
-                    final FileConfiguration config = this.config.getFile(wakeMobs.getDataFolder().getAbsolutePath(), "config.yml");
-                    final FileConfiguration mobsSettings = this.config.getFile(wakeMobs.getDataFolder().getAbsolutePath(), "mobs-settings.yml");
-                    this.config.setupSettings(config);
-                    this.config.setupItemSettings(config);
-                    this.config.setupMessages(config);
-                    this.config.setupListeners(config);
-                    this.config.setupEntities(mobsSettings);
+                    wakeMobs.reloadConfig();
+                    final FileConfiguration config = wakeMobs.getConfig();
+                    this.config.setupConfig(config);
+                    this.config.setupMobsSettings();
                     commandSender.sendMessage(this.config.getReloadMessage());
                 });
             } else {
                 commandSender.sendMessage(this.config.getHelpMessage());
             }
             return true;
+        } else {
+            commandSender.sendMessage("§a§lWakeMobs v" + wakeMobs.getDescription().getVersion() + "§fis running");
         }
 
         return false;
